279c3000e3bdf3abf02175d64bcfa503273267c4
Pit <p.gainanov@gmail.com>
2024-04-29 16:43:58 +0800

    refactor

--- file_one.py
+++ file_one.py
@@ -7,7 +7,3 @@
 # lorem ipsum dolor sit amet, consectetur adipis
 # lorem ipsum dolor sit amet, consectetur adipis
 # lorem ipsum dolor sit amet, consectetur adipis
-# test commit 1
-# test commit 2
-test 3
-# lorem ipsum dolor sit amet, consectetur adipis
